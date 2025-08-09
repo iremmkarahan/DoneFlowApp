a8c69ab6c157960ce8d02b1d19dc3e3df9de92d1
İrem Karahan <irem.karahan@tedu.edu.tr>
2025-08-09 16:44:24 +0300

    added the project files

---
+++ ToDoList/ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by İreemmmm on 9.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
